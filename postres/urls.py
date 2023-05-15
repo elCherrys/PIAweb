999f23f974ebf02b9744fb93b46b782b51ba954d
Yahir Orozco 3 <yahir23ab@outlook.com>
2023-05-15 12:42:21 -0600

    start

---
+++ postres/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('recetas/', views.recetas, name="recetas"),
+    path('saludables/', views.saludables, name="saludables"),
+    path('nogluten/', views.sin_gluten, name="sgluten"),
+    path('top10/', views.top_10, name="top10"),
+
+]
---
+++ postres/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def recetas(request):
+    return render(request,'index.html')
+
+def top_10(request):
+    return render(request,'index.html')
+
+def saludables(request):
+    return render(request,'index.html')
+
+def sin_gluten(request):
+    return render(request,'index.html')
